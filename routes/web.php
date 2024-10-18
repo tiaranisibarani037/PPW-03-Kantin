d321ff831964cd4c83a777aeb701cfea396186c9
desridabukke <desristenatal@gmail.com>
2024-10-18 16:34:05 +0700

    tampilan admin

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
 });
+Route::get('/adminpage', function () {
+    return view('admin');
+
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/adminpage', function () {
+    return view('admin');
+});
+
+Route::get('/createfood', function () {
+    return view('createfood');
+})->name('createfood');
